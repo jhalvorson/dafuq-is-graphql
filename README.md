22cb3690274ade665c7a37b34f4fb8e7edfb103e
Jamie Halvorson <jamie@halvorson.co.uk>
2018-04-28 22:44:27 +0100

    Updated README and main presentation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Dafuq is GraphQL](hero.jpg)
+![Dafuq is GraphQL](https://file-llahjbmikp.now.sh/)
 
 A presentation built using Spectactle for the Engineering team at [Signal](https://cellosignal.com).
 
--- src/presentation.js
+++ src/presentation.js
@@ -13,7 +13,9 @@ import {
   CodePane,
   BaseProps,
   Fill,
-  Image
+  Image,
+  Notes,
+  Appear,
 } from 'spectacle';
 import preloader from 'spectacle/lib/utils/preloader';
 import createTheme from 'spectacle/lib/themes/default';
@@ -22,7 +24,7 @@ import styled from 'styled-components';
 
 require('normalize.css');
 
-const images =  {
+const images = {
   live: require('./images/do-it-live.gif'),
 };
 
@@ -30,27 +32,27 @@ preloader(images);
 
 const theme = createTheme(
   {
-    primary: '#181920',
-    secondary: '#292633',
-    tertiary: '#3D394A',
-    quartenary: '#23212B',
+    primary: '#1B2B34',
+    secondary: '#343D46',
+    tertiary: '#4F5B66',
+    quartenary: '#65737E',
     text: '#FFFFFF',
   },
   {
     primary: 'Cerebri Sans, -apple-system, BlinkMacSystemFont, Open Sans, Helvetica Neue, sans-serif',
-    secondary: 'Source Code Pro',
+    secondary: 'Dank Mono',
   }
 );
 
-const Quote = styled(SQuote)`
+const Quote = styled(SQuote) `
   line-height: 1.4 !important;
 `;
 
-const BlockQuote = styled(SBlockQuote)`
+const BlockQuote = styled(SBlockQuote) `
   border-left: 7px solid #3D394A;
 `;
 
-const Cite = styled(SCite)`
+const Cite = styled(SCite) `
   margin-left: 40px;
 `;
 
@@ -67,29 +69,54 @@ export default class Presentation extends React.Component {
           <Heading size={1} fit lineHeight={1} textColor="text">
             Dafuq is GraphQL?
           </Heading>
-          <Text margin="50px 0 0" textColor="text" textFont="secondary" textSize="35px">
+          <Text margin="50px 0 0" textColor="text" italic textFont="secondary" textSize="35px">
             so 🔥 right now
           </Text>
         </Slide>
-        <Slide bgColor="primary" textColor="text">
-          <Heading size={6}>🤓</Heading>
-          <Heading size={6} textColor="text" >
-            Technically speaking
-          </Heading>
 
-          <BlockQuote textColor="text" margin="50px">
-            <Quote textColor="text" textSize="30px">
-              GraphQL is a query language for your API, and a server-side runtime for executing queries by using a type system you define for your data. GraphQL isn't tied to any specific database or storage engine and is instead backed by your existing code and data.
-            </Quote>
-            <Cite textColor="text">graphql.org</Cite>
-          </BlockQuote>
-
+        <Slide>
+          <Notes>
+            <h4>Before we dive in, let's take a step back and get some perspective</h4>
+            <p>We need to talk about REST</p>
+          </Notes>
+          <Heading textColor="text">Hold on</Heading>
+          <Text margin="50px 0 0" textColor="text" italic textFont="secondary" textSize="35px">Let's talk about REST</Text>
         </Slide>
 
+        <Slide textColor="text">
+          <Notes>
+            <ol>
+              <li></li>
+            </ol>
+          </Notes>
+
+          <Heading size={6}>REST APIs</Heading>
+          <Appear fid="1">
+            <Text>REST is one of the most common ways of interfacing with our data, normally via JSON</Text>
+          </Appear>
+          <Appear>
+            <Text>Each resource we request has its own endpoint, e.g /post/id/12</Text>
+          </Appear>
+        </Slide>
+
+        <Slide>
+          <Notes>
+            <ol>
+              <li>Single endpoint</li>
+            </ol>
+          </Notes>
+          <Heading size={6}>How does GraphQL compare?</Heading>
+          <List>
+            <ListItem>Single endpoint</ListItem>
+            <ListItem>The client controls the queries and mutations</ListItem>
+            <ListItem>The data is determined before hand</ListItem>
+          </List>
+        </Slide>
+
+
         <Slide bgColor="primary" textColor="text">
-          <Heading size={6}>😏</Heading>
-          <Heading size={6}  textColor="text">
-          What actually is it?
+          <Heading size={6} textColor="text">
+            What actually is it?
           </Heading>
           <List>
             <ListItem>Item 1</ListItem>
@@ -100,8 +127,8 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide bgColor="primary" textColor="text">
-          <Heading size={6}  textColor="text">
-           The benefits
+          <Heading size={6} textColor="text">
+            The benefits
           </Heading>
           <List>
             <ListItem>Well structured, auto-documenting API</ListItem>
@@ -112,8 +139,8 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide bgColor="primary" textColor="text">
-          <Heading size={6}  textColor="text">
-           How does it work?
+          <Heading size={6} textColor="text">
+            How does it work?
           </Heading>
           <List>
             <ListItem>Send empty JSON</ListItem>
@@ -124,8 +151,8 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide bgColor="primary" textColor="text">
-          <Heading size={6}  textColor="text">
-           Schemas & SDL
+          <Heading size={6} textColor="text">
+            Schemas & SDL
           </Heading>
 
           {/*<CodePane
